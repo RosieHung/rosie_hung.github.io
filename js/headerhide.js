f32a86bcaab27faaf2f5d69431c5d583828d3f65
RosieHung <77931899+RosieHung@users.noreply.github.com>
2021-01-25 22:00:55 +0800

    測試HTML

--- js/headerhide.js
+++ js/headerhide.js
@@ -1,12 +1,21 @@
 var bodyClass = document.body.classList,
 lastScrollY = 100;
 window.addEventListener('scroll', function(){
-  var st = this.scrollY;
-  // 判斷是向上捲動，而且捲軸超過 200px
-  if( st < lastScrollY) {
-    bodyClass.remove('hideUp');
-  }else{
-    bodyClass.add('hideUp');
-  }
-  lastScrollY = st;
+	if($(window).width() < 767)
+{
+    // JavaScript here 
+    // 當視窗寬度小於767px時執行
+} else {
+	  var st = this.scrollY;
+	  // 判斷是向上捲動，而且捲軸超過 200px
+	  if( st < lastScrollY) {
+	    bodyClass.remove('hideUp');
+	  }else{
+	    bodyClass.add('hideUp');
+	  }
+	  lastScrollY = st;
+    // JavaScript here
+    // 當視窗寬度不小於767px時執行
+}
 });
+
